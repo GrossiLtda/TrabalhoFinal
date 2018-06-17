3c54abf0809c7764b683c9eb839eebc7c0b1a813
Giulio Grossi <GiulioGrossi@macbook-pro-de-demo-5.local>
2018-06-16 22:54:51 -0300

    Initial Commit

---
+++ TrabalhoFInal/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TrabalhoFInal
+//
+//  Created by Giulio Grossi on 16/06/2018.
+//  Copyright © 2018 Giulio Grossi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
